5ad5ebaf05ad520acdb0b80d2b4947e75188de21
adequate <tenoba9@gmail.com>
2023-05-07 21:32:41 +0300

    Updates

---
+++ _app/immutable/chunks/1.5c01492f.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/error.svelte.c1cb5e1c.js";export{t as component};
---
+++ _app/immutable/chunks/2.e5ba2560.js
@@ -0,0 +1,1 @@
+import{default as t}from"../entry/_page.svelte.7e143a35.js";export{t as component};
